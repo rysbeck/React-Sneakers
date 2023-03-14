31b2c68ff422f0a86cd0937dc472449f6c3255bc
rysbeck <rysbeck1@gmail.com>
2023-03-14 23:19:54 +0400

    props, module, .map()

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Card from './components/Card'
 import Header from './components/Header'
 import Drawer from './components/Drawer'
 
+const arr = [ {title:'Men`s sneakers Nike Blazer Mid Suede', price: 12999,imageUrl:'./img/sneakers/1.jpg'},
+              {title:'Men`s sneakers Nike Air Max 270', price: 8999,imageUrl:'./img/sneakers/2.jpg'},
+              {title:'Men`s sneakers Nike Blazer Mid Suede', price: 9699,imageUrl:'./img/sneakers/3.jpg'},
+              {title:'Sneakers Puma X Aka Boku Future Rider', price: 8999,imageUrl:'./img/sneakers/4.jpg'},
+            ];
 function App() {
   return(
     <div className='wrapper clear'>
@@ -17,12 +22,15 @@ function App() {
           <input placeholder='Search...'></input>
         </div>
         </div>
-
-        
         <div className='d-flex'>
-          <Card/>
-        
-       
+          {arr.map((obj)=>(
+            <Card
+            title={obj.title}
+            price={obj.price}
+            imageUrl={obj.imageUrl}
+            onClick={()=>console.log(obj)}
+          />
+          ))}
         </div>
       </div>
       </div>
---
+++ src/components/Card/index.js
@@ -0,0 +1,25 @@
+import cardStyles from './Card.module.scss'
+
+function Card(props){
+    return(
+        <><div className={cardStyles.card}>
+            <div className='favorite'>
+                <img src='/img/heart-unliked.png' alt='Unliked' />
+            </div>
+            <img width={133} height={112} src={props.imageUrl} alt='Sneakers' />
+            <h5>{props.title}</h5>
+            <div className='d-flex justify-between align-center'>
+                <div className='d-flex flex-column '>
+                    <span>Price:</span>
+                    <b>{props.price} rub</b>
+                </div>
+                <button className='button' onClick={props.onClick}>
+                    <img width={11} height={11} src='/img/plus.svg' alt='Plus'></img>
+                </button>
+            </div>
+            </div></>
+    )
+}
+
+
+export default Card;
